7ad030b42d430d5dac1beeefdb7e147cd06666ac
Erick Adderly Leon Tejeda <erick.leon21@gmail.com>
2022-06-10 00:51:22 -0500

    New User Section Created

--- src/components/sections/NewUserSection.vue
+++ src/components/sections/NewUserSection.vue
@@ -23,6 +23,54 @@
         required
         @change="(e: any) => data.password = e.target.value"
       />
+      <DefaultInput
+        id="_name"
+        name="_name"
+        place-holder="Nombre"
+        type="text"
+        required
+        @change="(e: any) => data.name = e.target.value"
+      />
+       <DefaultInput
+        id="_sname"
+        name="_sname"
+        place-holder="Apellido"
+        type="text"
+        required
+        @change="(e: any) => data.sname = e.target.value"
+      />
+       <DefaultInput
+        id="_company"
+        name="_company"
+        place-holder="Empresa"
+        type="text"
+        required
+        @change="(e: any) => data.company = e.target.value"
+      />
+       <DefaultInput
+        id="_addr"
+        name="_addr"
+        place-holder="Direccion"
+        type="text"
+        required
+        @change="(e: any) => data.addr = e.target.value"
+      />
+       <DefaultInput
+        id="_email"
+        name="_email"
+        place-holder="Email"
+        type="email"
+        required
+        @change="(e: any) => data.email = e.target.value"
+      />
+       <DefaultInput
+        id="_phone"
+        name="_phone"
+        place-holder="Telefono"
+        type="text"
+        required
+        @change="(e: any) => data.phone = e.target.value"
+      />
       <DefaultButton text="Registrar Usuario" button-type="submit" />
       <DefaultButton text="Borrar Campos" button-type="reset" />
     </div>
@@ -33,6 +81,6 @@ import DefaultInput from '@/components/atoms/DefaultInput.vue';
 import DefaultButton from '@/components/atoms/DefaultButton.vue';
 import { ref, defineEmits } from 'vue';
 
-const data = ref({ username: '', password: '' });
+const data = ref({ username: '', password: '' , name: '', sname: '', company:'', addr:'', email:'', phone:''});
 defineEmits(["submit"]);
 </script>
